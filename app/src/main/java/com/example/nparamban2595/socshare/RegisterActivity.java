8c76e02612d2a5c84bef18d34befcd29527be299
NUHMAN PARAMBAN <nuhmanp@gmail.com>
2016-06-25 20:16:22 -0400

    2nd update
    2nd update

--- app/src/main/java/com/example/nparamban2595/socshare/RegisterActivity.java
+++ app/src/main/java/com/example/nparamban2595/socshare/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.nparamban2595.socshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final EditText et = (EditText)findViewById(R.id.editText);
+        et.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (!hasFocus){
+                    et.setError("username already taken");
+                }
+            }
+        });
+    }
+    public void registerProcess(View view){
+
     }
 }
